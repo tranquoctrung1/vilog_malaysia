9fcb3105e436da960460840a349dffe8bc74eaf7
tranquoctrung1 <tranquoctrung.12k9@gmail.com>
2025-10-30 14:55:57 +0700

    update code

--- controller/login.js
+++ controller/login.js
@@ -1,9 +1,20 @@
 const settings = require('../settings.json');
 
 module.exports.login = function (req, res) {
-    res.render('login', {title: settings.title, companyName: settings.CompanyName, errorLogin: ""})
-}
+    res.render('login', {
+        title: settings.title,
+        companyName: settings.CompanyName,
+        errorLogin: '',
+    });
+};
 
 module.exports.validationLoggin = function (req, res) {
-        res.redirect('/')
-}
+    const userAgent = req.headers['user-agent'] || '';
+    const isMobile = /mobile|android|iphone|ipad|ipod/i.test(userAgent);
+
+    if (isMobile) {
+        res.redirect('/dashboardVilog');
+    } else {
+        res.redirect('/');
+    }
+};
